bdf35b68a4513ac912ca9050f9a6d2b62820b79f
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2014-01-14 15:27:46 +0000

    Package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='DQXTableUtils',
+    version='0.1.0',
+    packages=['DQXTableUtils'],
+    description='Useful towel-related stuff.',
+    install_requires=[
+        "xlrd>=0.9.2",
+    ],
+)
